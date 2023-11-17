c7a53e98f8d22b1a6f8a892454b7a8ac0c213019
BQX <b22050106@njupt.edu.cn>
2023-11-17 08:29:04 +0800

    feat: setting #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
-import AppRouter from "./router/routerConfig";
+// import AppRouter from "./router/routerConfig";
 import { setupIonicReact } from '@ionic/react';
 import '@ionic/react/css/core.css';
+import Home from "./pages/home/home";
+import { IonApp } from "@ionic/react";
+import AppRouter from './router/routerConfig.jsx'
 
 setupIonicReact();
 
 const App = () => {
   return (
-    <ion-app>
+    <IonApp>
       <AppRouter/>
-    </ion-app>
+    </IonApp>
   )
 };
 
