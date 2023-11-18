c4131981cf0bec8d022d2fbaa59a85ccee9c54ac
BQX <b22050106@njupt.edu.cn>
2023-11-18 23:54:30 +0800

    add dist

--- src/reducers/themeReducer.jsx
+++
@@ -1,8 +0,0 @@
-export const themeReducer = (state, action) => {
-  switch (action.type) {
-    case "TOGGLE_THEME":
-      return state === "light" ? "dark" : "light";
-    default:
-      return state;
-  }
-};
